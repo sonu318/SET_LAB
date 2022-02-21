e058736dc0028db061dbc3c14cdeb8beb2adf376
sonu318 <91945935+sonu318@users.noreply.github.com>
2022-02-21 21:53:32 +0530

    Update login.php
    file updated

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login Page</title>
+	<title>Login Page 1</title>
 	<link rel="stylesheet" type="text/css" href="bootstrap-4.4.1/css/bootstrap.min.css">
   	<script type="text/javascript" src="bootstrap-4.4.1/js/juqery_latest.js"></script>
   	<script type="text/javascript" src="bootstrap-4.4.1/js/bootstrap.min.js"></script>
